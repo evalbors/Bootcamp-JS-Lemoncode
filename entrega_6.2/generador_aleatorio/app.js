c04774accca906bd5baa2ff8391cf71efd1cc0ee
evalbors <evalbors@gmail.com>
2022-10-15 09:35:36 +0200

    last evolutive

--- entrega_6.2/generador_aleatorio/app.js
+++ entrega_6.2/generador_aleatorio/app.js
@@ -1,19 +1,14 @@
-// Vamos a construir un generador aleatorio que permita escoger, 
-// de forma aleatoria, una cantidad determinada de números 
-// de entre un mínimo y un máximo. Para entenderlo mejor, 
-// os daremos la firma que debe tener la función:
-
 var randomPick = (n, min, max) => {
     const range = max - min + 1;
-    var randomList =[];
-    for(var i = 0; randomList.length <= n; i++){
-        var randomNumber = Math.floor(Math.random() * range);
-        if(!isRepeated(randomList,randomNumber)){
-            randomList.push(randomNumber)
-        }      
+    var randomList = [];
+    while (randomList.length != n) {
+      var randomNumber = Math.floor(Math.random() * range);
+      if (!isRepeated(randomList, randomNumber)) {
+        randomList.push(randomNumber);
+      }
     }
     return randomList;
-}
+};
 
 var isRepeated = (arr, number) => {
     for(var i = 0; i <= arr.length; i++){
@@ -24,7 +19,7 @@ var isRepeated = (arr, number) => {
     return false;
 }
 
-console.log(randomPick(10, 1, 10));
+console.log(randomPick(10, 1, 100));
 
 
 // De esta forma, para escoger 10 números aleatorios entre el 1 y el 100, usaremos la función anterior del siguiente modo:
--- entrega_6.3/app.js
+++ entrega_6.3/app.js
@@ -1,27 +1,114 @@
-var square = (n, char) => { 
-var draw = '';
-    for(var i = 0; i <= n; i++){
-        //console.log(line)
-        for(var j = 0; j <= n; j++){
-            draw += char;
-            //console.log(draw)
-       }
-       draw += '\n';
+/* var square = (n, char) => {
+  var draw = "";
+  for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= n; j++) {
+      draw += char;
     }
-    return draw;
-}
+    draw += "\n";
+  }
+  return draw;
+};
 
-console.log(square(5, '*'));
+console.log(square(5, "* "));
 
+var drawLine = (n, char) => {
+  var line = "";
+  for (var i = 1; i <= n; i++) {
+    line += char;
+  }
+  return (line += "\n");
+};
+var squareWithBorder = (n, charBorder, inner) => {
+  var draw = "";
+  const topLine = drawLine(n, charBorder);
 
-// *
-// *
-// *
-// *
-// *
+  for (var i = 1; i <= n - 2; i++) {
+    for (var j = 1; j <= n; j++) {
+      var result = j === 1 || j === n ? charBorder : inner;
+      draw += result;
+    }
+    draw += "\n";
+  }
 
-// *
-// *
-// *
-// *
-// *
+  const bottomLine = drawLine(n, charBorder);
+
+  return topLine + draw + bottomLine;
+};
+console.log(squareWithBorder(6, "* ", "/ "));
+
+var squareDiagonalLeftRight = (n, charDiagonal, charUp, charDown) => {
+  var draw = "";
+  for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= n; j++) {
+      var whichChar = i < j ? charUp : charDown;
+      var result = i === j ? charDiagonal : whichChar;
+      draw += result;
+    }
+    draw += "\n";
+  }
+  return draw;
+};
+console.log(squareDiagonalLeftRight(8, "🐠 ", "💦 ", "📌 "));
+
+var squareDiagonalRightLeft = (n, charDiagonal, charUp, charDown) => {
+  var draw = "";
+  for (var i = 1; i <= n; i++) {
+    for (var j = n; j >= 1; j--) {
+      var whichChar = i < j ? charUp : charDown;
+      var result = i === j ? charDiagonal : whichChar;
+      draw += result;
+    }
+    draw += "\n";
+  }
+  return draw;
+};
+console.log(squareDiagonalRightLeft(8, "🐷 ", "👆🏽 ", "👇🏽 "));
+
+var halfDiamond = (n, char) => {
+  var draw = "";
+  for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= n; j++) {
+      if (i >= j) {
+        draw += char;
+      }
+    }
+    draw += "\n";
+  }
+  var drawNext = "";
+
+  for (var k = 1; k <= n - 1; k++) {
+    for (var l = n - 1; l >= 1; l--) {
+      if (k <= l) {
+        drawNext += char;
+      }
+    }
+    drawNext += "\n";
+  }
+
+  return draw + drawNext;
+};
+console.log(halfDiamond(6, "♢ "));
+ */
+
+var pyramid = (n, char) => {
+  var draw = "";
+  var body = "";
+  var vertex = "";
+  var mid = Math.round(n / 2);
+
+  for (var i = 1; i <= n; i++) {
+    // console.log(i === mid)
+    vertex = i === mid ? char : "";
+    console.log(mid);
+    for (var j = mid ; j <= mid; j++) {
+      console.log(n, j);
+      body = char += "\n";
+    }
+  }
+
+  draw = vertex + body; 
+
+  return draw;
+};
+
+console.log(pyramid(5, "💎 "));
