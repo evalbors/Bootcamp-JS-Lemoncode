c6c66a3e90c1dd59642ce7313f2d1a6b9a354055
Eva <eva@MacBook-MacBook-Pro-de-Eva.local>
2022-11-06 10:23:06 +0100

    delivery 6.3: pyarmid and diamant with loops

--- entrega_6.3/app.js
+++ entrega_6.3/app.js
@@ -1,25 +1,25 @@
-/* var square = (n, char) => {
-  var draw = "";
+var square = (n, char) => {
+  var draw = '';
   for (var i = 1; i <= n; i++) {
     for (var j = 1; j <= n; j++) {
       draw += char;
     }
-    draw += "\n";
+    draw += '\n';
   }
   return draw;
 };
-
-console.log(square(5, "* "));
+console.log(square(5, '* '));
 
 var drawLine = (n, char) => {
-  var line = "";
+  var line = '';
   for (var i = 1; i <= n; i++) {
     line += char;
   }
-  return (line += "\n");
+  return (line += '\n');
 };
+
 var squareWithBorder = (n, charBorder, inner) => {
-  var draw = "";
+  var draw = '';
   const topLine = drawLine(n, charBorder);
 
   for (var i = 1; i <= n - 2; i++) {
@@ -27,54 +27,54 @@ var squareWithBorder = (n, charBorder, inner) => {
       var result = j === 1 || j === n ? charBorder : inner;
       draw += result;
     }
-    draw += "\n";
+    draw += '\n';
   }
 
   const bottomLine = drawLine(n, charBorder);
 
   return topLine + draw + bottomLine;
 };
-console.log(squareWithBorder(6, "* ", "/ "));
+console.log(squareWithBorder(6, '* ', '/ '));
 
 var squareDiagonalLeftRight = (n, charDiagonal, charUp, charDown) => {
-  var draw = "";
+  var draw = '';
   for (var i = 1; i <= n; i++) {
     for (var j = 1; j <= n; j++) {
       var whichChar = i < j ? charUp : charDown;
       var result = i === j ? charDiagonal : whichChar;
       draw += result;
     }
-    draw += "\n";
+    draw += '\n';
   }
   return draw;
 };
-console.log(squareDiagonalLeftRight(8, "🐠 ", "💦 ", "📌 "));
+console.log(squareDiagonalLeftRight(8, '🐠 ', '💦 ', '📌 '));
 
 var squareDiagonalRightLeft = (n, charDiagonal, charUp, charDown) => {
-  var draw = "";
+  var draw = '';
   for (var i = 1; i <= n; i++) {
     for (var j = n; j >= 1; j--) {
       var whichChar = i < j ? charUp : charDown;
       var result = i === j ? charDiagonal : whichChar;
       draw += result;
     }
-    draw += "\n";
+    draw += '\n';
   }
   return draw;
 };
-console.log(squareDiagonalRightLeft(8, "🐷 ", "👆🏽 ", "👇🏽 "));
+console.log(squareDiagonalRightLeft(8, '🐷 ', '👆🏽 ', '👇🏽 '));
 
 var halfDiamond = (n, char) => {
-  var draw = "";
+  var draw = '';
   for (var i = 1; i <= n; i++) {
     for (var j = 1; j <= n; j++) {
       if (i >= j) {
         draw += char;
       }
     }
-    draw += "\n";
+    draw += '\n';
   }
-  var drawNext = "";
+  var drawNext = '';
 
   for (var k = 1; k <= n - 1; k++) {
     for (var l = n - 1; l >= 1; l--) {
@@ -82,33 +82,57 @@ var halfDiamond = (n, char) => {
         drawNext += char;
       }
     }
-    drawNext += "\n";
+    drawNext += '\n';
   }
 
   return draw + drawNext;
 };
-console.log(halfDiamond(6, "♢ "));
- */
+console.log(halfDiamond(6, '♢ '));
 
-var pyramid = (n, char) => {
-  var draw = "";
-  var body = "";
-  var vertex = "";
-  var mid = Math.round(n / 2);
-
+var drawPyramide = (n, char) => {
+  var draw = '';
+  // External loop
   for (var i = 1; i <= n; i++) {
-    // console.log(i === mid)
-    vertex = i === mid ? char : "";
-    console.log(mid);
-    for (var j = mid ; j <= mid; j++) {
-      console.log(n, j);
-      body = char += "\n";
+    // printing spaces
+    for (var j = 1; j <= n - i; j++) {
+      draw += '   ';
     }
+    // printing star
+    for (var k = 0; k < 2 * i - 1; k++) {
+      draw += char;
+    }
+    draw += '\n';
   }
-
-  draw = vertex + body; 
-
   return draw;
 };
+console.log(drawPyramide(4, ' * '));
 
-console.log(pyramid(5, "💎 "));
+var displayDiamont = (n, char) => {
+  var draw = '';
+  // Upside pyramid
+  for (var i = 1; i <= n; i++) {
+    // printing spaces
+    for (var j = n; j > i; j--) {
+      draw += ' ';
+    }
+    // printing star
+    for (var k = 0; k < i * 2 - 1; k++) {
+      draw += char;
+    }
+    draw += '\n';
+  }
+  // downside pyramid
+  for (var i = 1; i <= n - 1; i++) {
+    // printing spaces
+    for (var j = 0; j < i; j++) {
+      draw += ' ';
+    }
+    // printing star
+    for (var k = (n - i) * 2 - 1; k > 0; k--) {
+      draw += char;
+    }
+    draw += '\n';
+  }
+  return draw;
+};
+console.log(displayDiamont(4, '*'));
